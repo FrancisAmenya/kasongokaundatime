8ea75ee3c89f537eafe61685669e7f6f427925b4
FrancisAmenya <amenyafrancis@gmail.com>
2025-01-02 22:53:27 +0300

    switched Crouch() and Crouch2() Algorithms

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -43,11 +43,11 @@ public class ControlJugador : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.DownArrow)) //Si pulsamos abajo, el dinosaurio se agacha
             {
-                Crouch();
+                Crouch2();
             }
             else if (!Input.GetKey(KeyCode.DownArrow)) //Y si no lo estamos pulsando se pone en pié
             {
-                Crouch2();
+                Crouch();
             }
 
 
